a4844a446d6c47ca6d0eb7a7fd4c12de50043992
Adrian Selva <adrianjselva@gmail.com>
2022-05-24 18:43:26 -0400

    Flesh out plot definition

--- include/plot/Plot.h
+++ include/plot/Plot.h
@@ -6,11 +6,15 @@
 #include "dataset/NormalizedDataset.h"
 
 class Plot {
-    explicit Plot(float* x, int x_length, float* y, int y_length);
+    explicit Plot(float* x, int x_length, float* y, int y_length, void* texture, int texture_width, int texture_height);
+    explicit Plot(void* texture, int texture_width, int texture_height);
 
     void render();
 private:
     std::unique_ptr<NormalizedDataset> dataset_;
+    void* texture_;
+    int texture_width_;
+    int texture_height_;
 };
 
 
--- src/plot/Plot.cpp
+++ src/plot/Plot.cpp
@@ -1,7 +1,21 @@
 #include "plot/Plot.h"
 
-Plot::Plot(float *x, int x_length, float *y, int y_length)
-          : dataset_(std::make_unique<NormalizedDataset>(x, x_length, y, y_length)) {
+Plot::Plot(float *x, int x_length, float *y, int y_length, void* texture, int texture_width, int texture_height)
+          : dataset_(std::make_unique<NormalizedDataset>(x, x_length, y, y_length)),
+            texture_width_(texture_width),
+            texture_height_(texture_height),
+            texture_(texture){
+}
 
+Plot::Plot(void* texture, int texture_width, int texture_height)
+          : texture_(texture),
+            texture_width_(texture_width),
+            texture_height_(texture_height),
+            dataset_(std::make_unique<NormalizedDataset>(texture_width, texture_height, 0, 0)){}
+
+void Plot::render() {
+    if(!texture_) {
+        return;
+    }
 }
     
